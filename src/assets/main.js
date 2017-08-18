1f587b9cbf32e0614ab27878895421dc6310003e
Pavel Kurskiy <pavelkurskiy@Pavels-MacBook-Air.local>
2017-08-18 19:02:57 +0300

    commit

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,37 @@
 $(function() {
+  $.ajax('https://www.codeschool.com/users/3222765.json', {
+    dataType: 'jsonp',
+    success: function(response) {
+      addCourses(response.courses.completed);
+      //console.log('response',response.courses.completed);
+    }
+  });
 
-  // your code will go here
+  function addCourses(courses) {
 
+    var $badges = $('#badges');
+
+    courses.forEach(function(course) {
+      var $course = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 >', {
+        text:course.title
+      }).appendTo($course);
+
+      $('<img >', {
+        src:course.badge
+      }).appendTo($course);
+
+      $('<a >', {
+        href:course.url,
+        'class': 'btn btn-primary',
+        text: 'See Course',
+        target: '_blank'
+      }).appendTo($course);
+    })
+
+
+  }
 });
